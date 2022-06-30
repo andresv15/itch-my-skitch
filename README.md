2c5ba48f03a81223d3646527032b0fabff7d6a9b
Andres Valencia <95753539+andresv15@users.noreply.github.com>
2022-06-30 03:10:03 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# itch-my-skitch
+# Etch-A-Sketch
+THis project is from the Odin Project. the assignment was to create a Etch-a-Sketch, with the fundamentals we have learn through the circulumn of the Odin Project's. Tocomplete this project I will utulize html, css,  javascript and showcast Dom Manuplation and Events to make the buttons do wjat they are suppose to be doing. The more I continue to get done with the circulumn, i will continue to come nack to project and add it more to it. 
---
+++ script.js
@@ -0,0 +1,171 @@
+const cont = document.querySelector('.container');
+const slider = document.querySelector('#slider');
+const output = document.querySelector('#output');
+const resetBtn = document.querySelector('.reset');
+const regularBtn = document.querySelector('.regular');
+const randomBtn = document.querySelector('.random');
+const darkenBtn = document.querySelector('.darken');
+let currentStyle = 'regular';
+let boxes = document.querySelectorAll('.smol');
+
+makeDivs(output.value);
+
+
+//layout for setting up the slider.r
+slider.addEventListener('input', (e) => output.value = e.target.value);
+slider.addEventListener('mouseup', () => reset());
+output.addEventListener('input', (e) => {
+    slider.value = e.target.value;
+    reset();
+});
+
+//Convertion between rgb color = hexadecimal, 
+//Had to search for the format on this one to get grid going.
+
+function rgbToHex(col)
+{
+    if(col.charAt(0)=='r')
+    {
+        col=col.replace('rgb(','').replace(')','').split(',');
+        var r=parseInt(col[0], 10).toString(16);
+        var g=parseInt(col[1], 10).toString(16);
+        var b=parseInt(col[2], 10).toString(16);
+        r=r.length==1?'0'+r:r; g=g.length==1?'0'+g:g; b=b.length==1?'0'+b:b;
+        var colHex='#'+r+g+b;
+        return colHex;
+    };
+};
+
+const hexArray = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+function randomColor() {
+    let randomColorString = '#';
+    for (let i = 0; i < 6; i++) {
+        let value = hexArray[Math.floor(Math.random() * 16)];
+        randomColorString += value;
+    }
+    return randomColorString
+};
+
+//Some code for darkening(or lightening) a color by an amount
+//Still pretty confused how it properly works and l those good stuff.
+//ill understand a little more with time.
+function darkenColor(color, amount) {
+    let usePound = false;
+    if (color[0] == '#') {
+        color = color.slice(1);
+        usePound = true;
+    }
+
+    let num = parseInt(color,16);
+    
+    let r = (num >> 16) + amount;
+    if (r > 255) r = 255;
+    else if (r < 0) r = 0;
+
+    let b = ((num >> 8) & 0x00FF) + amount;
+    if (b > 255) b = 255;
+    else if (b < 0) b = 0;
+
+    let g = (num & 0x0000FF) + amount;
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+
+    return (usePound?'#':'') + String('000000' + (g | (b << 8) | (r << 16)).toString(16)).slice(-6);
+};
+//function creating a min and max number for the grid.
+function makeDivs(rows) {
+    if (rows > 100) {
+        return makeDivs(prompt('Please select a number between 1-100'))
+    } else {
+        for (i=1; i<=rows * rows; i++) {
+            let width = 480 / rows;
+            const div = document.createElement('div');
+            div.classList.add('smol');
+            div.id = 'box' + i;
+            div.style.width = `${width}px`;
+            div.style.height = `${width}px`;
+            cont.appendChild(div);
+        };
+    };
+};
+//function for the grid to get  filled by hovering over.
+function fillBoxes() {
+    currentStyle = 'regular';
+    boxes = document.querySelectorAll('.smol');
+    boxes.forEach((div) => {
+        div.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = 'black';
+        });
+    });
+};
+
+function fillDarken() {
+    currentStyle = 'darken';
+    boxes = document.querySelectorAll('.smol');
+    boxes.forEach((div) => div.style.backgroundColor = '#FFFFFF');
+    boxes.forEach((div) => {
+        div.addEventListener('mouseover', (e) => {
+            let currentColor = e.target.style.backgroundColor;
+            currentColor = rgbToHex(currentColor);
+            e.target.style.backgroundColor = `${darkenColor(currentColor, -30)}`;
+        });
+    });
+};
+
+function fillRandom() {
+    currentStyle = 'random';
+    boxes = document.querySelectorAll('.smol');
+    boxes.forEach((div) => {
+        div.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = `${randomColor()}`;
+        });
+    });
+};
+//function for once another button is clicked grid clears.
+function removeBoxes() {
+    boxes = document.querySelectorAll('.smol');
+    boxes.forEach((div) => {
+        div.remove();
+    });
+};
+//function  to get grid clear. 
+function reset() {
+    removeBoxes();
+    makeDivs(output.value);
+    switch(currentStyle) {
+        case('regular'):
+            fillBoxes();
+            break;
+        case('darken'):
+            fillDarken();
+            break;
+        case('random'):
+            fillRandom();
+            break;
+        default:
+            console.log('Something went wrong!');
+    };
+};
+
+//function for the buttons to actually work. 
+resetBtn.addEventListener('click', () => reset());
+
+regularBtn.addEventListener('click', () => {
+    removeBoxes();
+    makeDivs(output.value);
+    fillBoxes();
+});
+
+darkenBtn.addEventListener('click', () => {
+    removeBoxes();
+    makeDivs(output.value);
+    fillDarken();
+});
+
+randomBtn.addEventListener('click', () => {
+    removeBoxes();
+    makeDivs(output.value);
+    fillRandom();
+});
+
+fillBoxes();
